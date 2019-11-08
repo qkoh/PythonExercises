557ea5cd5dcdcbdf106ae9f80e59ca202285aead
qkoh <57423772+qkoh@users.noreply.github.com>
2019-11-08 15:59:56 +0800

    Add files via upload

---
+++ exercise7.py
@@ -0,0 +1,18 @@
+# Challenge - Classes Exercise
+
+# Add a method to the Car class called age
+# that returns how old the car is (2019 - year)
+
+# *Be sure to return the age, not print it
+import datetime
+
+
+class Car:
+
+    def __init__(self, year, make, model):
+        self.year = year
+        self.make = make
+        self.model = model
+
+    def age(self):
+        return datetime.datetime.now().year - self.year
